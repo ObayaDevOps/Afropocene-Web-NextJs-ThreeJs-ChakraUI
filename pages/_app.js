997fad6dfc48c3139095e26a6bcea8601761e0a8
ObayaDevOps <obayajamesdralega@gmail.com>
2024-10-03 18:29:24 +0300

    removed ethereum packs

--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,14 @@ import WithSubnavigation from '../components/utils/navbar'
 import FooterLargeWithNewsletter from '../components/utils/footer' 
 import theme from '../components/utils/theme'
 
-import { createClient, configureChains, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
 import { SessionProvider } from "next-auth/react";
-import { mainnet } from "wagmi/chains";
 
 
 import { Analytics } from '@vercel/analytics/react';
 
-const { provider, webSocketProvider } = configureChains(
-  [mainnet],
-  [publicProvider()]
-);
-
-const client = createClient({
-  provider,
-  webSocketProvider,
-  autoConnect: true,
-});
-
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WagmiConfig client={client}>
       <SessionProvider session={pageProps.session} refetchInterval={0}>
 
       <ChakraProvider theme={theme}>
@@ -38,7 +23,6 @@ function MyApp({ Component, pageProps }) {
       </ChakraProvider>      
     
     </SessionProvider>
-    </WagmiConfig>
   );
 }
 
