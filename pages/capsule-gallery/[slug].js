731e03b2939938bc9aff731e328c87e96dc96b73
ObayaDevOps <obayajamesdralega@gmail.com>
2025-12-09 11:32:28 +0300

    added dynamic routes for 404s

--- pages/capsule-gallery/[slug].js
+++ pages/capsule-gallery/[slug].js
@@ -165,7 +165,7 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug) => ({ params: { slug } })),
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
--- pages/exhibitions/[slug].js
+++ pages/exhibitions/[slug].js
@@ -157,7 +157,7 @@ export async function getStaticPaths() {
 
     return {
         paths: paths.map((slug) => ({params: {slug}})),
-        fallback: false,
+        fallback: 'blocking',
     }
 }
 
